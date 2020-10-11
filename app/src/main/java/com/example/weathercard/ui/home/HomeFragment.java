825dfbff968426b3942035a899cf95622580f31f
Milkyo <dp221125@naver.com>
2020-10-11 21:57:41 +0900

    feat: rxjava 추가

--- app/src/main/java/com/example/weathercard/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/weathercard/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,21 +13,15 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.weathercard.APIData.Weather;
 import com.example.weathercard.MainDataAdapter;
 import com.example.weathercard.R;
-import com.example.weathercard.network.WeatherAPIProvider;
 
 import java.util.ArrayList;
 
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
-import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class HomeFragment extends Fragment {
 
@@ -34,9 +29,7 @@ public class HomeFragment extends Fragment {
     private RecyclerView mainRecyclerView;
     private RecyclerView.LayoutManager layoutManager;
 
-    final String CONSUMER_KEY = "dj0yJmk9bTYzMDA5WU1GREEzJmQ9WVdrOU5YQkpWMlZqYUdzbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWI3";
-    final String CONSUMER_SECRET = "d9be33bf0bf6fd581a87f645adf1c55f98c00821";
-    final String baseUrl = "https://weather-ydn-yql.media.yahoo.com/forecastrss/";
+    private final CompositeDisposable disposables = new CompositeDisposable();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -68,34 +61,25 @@ public class HomeFragment extends Fragment {
 
         System.out.println("CALL");
 
-        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new SigningInterceptor(consumer))
-                .build();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build();
-
-        WeatherAPIProvider provider = retrofit.create(WeatherAPIProvider.class);
-        provider.getData("37.372", "-122.038", "json").enqueue(new Callback<Weather>() {
-            @Override
-            public void onResponse(Call<Weather> call, Response<Weather> response) {
-                if (response.isSuccessful()) {
-                    Weather weather = response.body();
-
-                    System.out.println(weather.getLocation().getCity());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Weather> call, Throwable t) {
-                Log.e("fail", t.toString());
-            }
-        });
+        disposables.add( homeViewModel.fetchData()
+                .subscribe( (weather, throwable) -> {
+                    if (throwable == null) {
+                        System.out.println(weather.getLocation().getCity());
+                    } else {
+                        Toast toast = Toast.makeText(getActivity(), throwable.toString(), LENGTH_SHORT);
+                        toast.show();
+                    }
+                }));
 
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(!disposables.isDisposed()){
+            disposables.dispose();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/weathercard/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/weathercard/ui/home/HomeViewModel.java
@@ -1,12 +1,31 @@
 package com.example.weathercard.ui.home;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.weathercard.APIData.Weather;
+import com.example.weathercard.network.WeatherAPIProvider;
+
+import io.reactivex.rxjava3.core.Single;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+
+    final String CONSUMER_KEY = "dj0yJmk9bTYzMDA5WU1GREEzJmQ9WVdrOU5YQkpWMlZqYUdzbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWI3";
+    final String CONSUMER_SECRET = "d9be33bf0bf6fd581a87f645adf1c55f98c00821";
+    final String baseUrl = "https://weather-ydn-yql.media.yahoo.com/forecastrss/";
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
@@ -15,5 +34,41 @@ public class HomeViewModel extends ViewModel {
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public Single<Weather> fetchData() {
+       return Single.create( emitter ->  {
+
+           OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+
+           OkHttpClient client = new OkHttpClient.Builder()
+                   .addInterceptor(new SigningInterceptor(consumer))
+                   .build();
+
+           Retrofit retrofit = new Retrofit.Builder()
+                   .baseUrl(baseUrl)
+                   .addConverterFactory(GsonConverterFactory.create())
+                   .client(client)
+                   .build();
+
+           WeatherAPIProvider provider = retrofit.create(WeatherAPIProvider.class);
+           provider.getData("37.372", "-122.038", "json").enqueue(new Callback<Weather>() {
+               @Override
+               public void onResponse(Call<Weather> call, Response<Weather> response) {
+                   if (response.isSuccessful()) {
+                       Weather weather = response.body();
+                       emitter.onSuccess(weather);
+                   } else {
+                       Log.e("onResponse at FetchData", response.headers().toString());
+                   }
+               }
+
+               @Override
+               public void onFailure(Call<Weather> call, Throwable t) {
+                   Log.e("Fetch Error", t.toString());
+                   emitter.onError(t);
+               }
+           });
+        });
     }
 }
